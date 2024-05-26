3e4300050b73763400dfae4e63d1aa28a7461119
ShivamSikotra11 <sikotra756@gmail.com>
2024-05-26 18:13:23 +0530

    final

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { useUserContext } from '../store/userContext';
 
 const TodoHeader = () => {
-  const {loggedIn,getLogOut} = useUserContext();         
+  const {loggedIn,getLogOut,getUser} = useUserContext(); 
+  const name = getUser().name.split(" ")[0];
   return (
     <div className='flex justify-between items-center px-8 font-sora  bg-primary py-4 '>
       <div className=' text-white  text-center  text-3xl font-semibold' >
@@ -11,7 +12,7 @@ const TodoHeader = () => {
      
       <div className='flex  space-x-4' >
         <div className="text-white text-xl" onClick={getLogOut} >LogOut</div>
-        <div className="text-white text-xl">Shivam</div>
+        <div className="text-white text-xl capitalize">{name}</div>
       </div>
  
     </div>
